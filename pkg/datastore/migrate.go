96001d776c5493da9b1095c6b0dfa3d284da2e1f
Stefan Majer <stefan.majer@f-i-ts.de>
2022-11-05 12:39:06 +0100

    enable migrate again, with sample only

--- pkg/datastore/migrate.go
+++ pkg/datastore/migrate.go
@@ -1,8 +1,10 @@
 package datastore
 
 import (
+	"context"
 	"database/sql"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/lopezator/migrator"
 	healthv1 "github.com/metal-stack/masterdata-api/api/grpc/health/v1"
 	v1 "github.com/metal-stack/masterdata-api/api/v1"
@@ -11,28 +13,33 @@ import (
 )
 
 // MigrateDB applies necessary DB Migrations.
-func (ds *datastore[E]) MigrateDB(healthServer *health.Server) error {
+func MigrateDB(log *zap.SugaredLogger, db *sqlx.DB, healthServer *health.Server) error {
 
 	m, err := migrator.New(
 		migrator.Migrations(
 			// migrations will be applied and remembered in this order, so always add new migrations below if necessary
 			&migrator.Migration{
-				Name: "Consolidate History for Tenant and Project",
+				Name: "Sample Migration for Tenant",
 				Func: func(tx *sql.Tx) error {
-					entities := []any{&[]v1.Project{}, &[]v1.Tenant{}}
-					for _, e := range entities {
-						err := ds.consolidateHistory(tx, e)
-						if err != nil {
-							ds.log.Error("error consolidate history", zap.Error(err))
-							return err
-						}
+					ts, err := NewPostgresStorage(log.Desugar(), db, &v1.Tenant{})
+					if err != nil {
+						return err
 					}
+
+					tenants, _, err := ts.Find(context.Background(), nil, nil)
+					if err != nil {
+						return err
+					}
+					for _, tenant := range tenants {
+						log.Debugw("migrate", "tenant", tenant)
+					}
+
 					return nil
 				},
 			},
 		),
 		migrator.WithLogger(migrator.LoggerFunc(func(msg string, args ...any) {
-			ds.log.Sugar().Infof(msg, args...)
+			log.Infof(msg, args...)
 		})),
 	)
 	if err != nil {
@@ -40,60 +47,10 @@ func (ds *datastore[E]) MigrateDB(healthServer *health.Server) error {
 	}
 
 	// Migrate up
-	if err := m.Migrate(ds.db.DB); err != nil {
+	if err := m.Migrate(db.DB); err != nil {
 		return err
 	}
 
 	healthServer.SetServingStatus("migratedb", healthv1.HealthCheckResponse_SERVING)
-	return nil
-}
-
-// consolidateHistory ensures, that for each VersionedJSONEntity there is at least one "created"-row in the history table.
-// The type of entities to consolidate is specified by the given pointer to a slice of entities.
-func (ds *datastore[E]) consolidateHistory(tx *sql.Tx, entitySlicePtr any) error {
-
-	// entitySliceV := reflect.ValueOf(entitySlicePtr)
-	// if entitySliceV.Kind() != reflect.Ptr || entitySliceV.Elem().Kind() != reflect.Slice {
-	// 	return fmt.Errorf("entity argument must be a slice address")
-	// }
-	// entitySliceElem := entitySliceV.Elem()
-	// entitySliceElementType := entitySliceElem.Type().Elem()
-
-	// filter := make(map[string]any)
-	// _, err := ds.Find(context.Background(), filter, nil, entitySlicePtr)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// for i := 0; i < entitySliceElem.Len(); i++ {
-	// 	vpi := entitySliceElem.Index(i).Addr().Interface()
-	// 	enityVe, ok := vpi.(Entity)
-	// 	if !ok {
-	// 		return fmt.Errorf("element type must implement VersionedJSONEntity-Interface, was %T", vpi)
-	// 	}
-
-	// 	historyVe, ok := reflect.New(entitySliceElementType).Interface().(Entity)
-	// 	if !ok {
-	// 		return fmt.Errorf("element type must implement VersionedJSONEntity-Interface")
-	// 	}
-
-	// 	// check if we already have a "created" row for this entity in history
-	// 	err = ds.GetHistoryCreated(context.Background(), enityVe.GetMeta().Id, historyVe)
-	// 	if err == nil {
-	// 		continue
-	// 	}
-
-	// 	if !errors.As(err, &NotFoundError{}) {
-	// 		return err // some sort of technical error stops us
-	// 	}
-
-	// 	// consolidate history by inserting the "created" row in history at the correct date
-	// 	entityCreatedPbTs := enityVe.GetMeta().CreatedTime
-	// 	entityCreated := entityCreatedPbTs.AsTime()
-	// 	err := ds.insertHistory(enityVe, opCreate, entityCreated, tx)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
 	return nil
 }
