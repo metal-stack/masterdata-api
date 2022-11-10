8575fc127626b7dcf3ed35784b02bdf242caf0be
Stefan Majer <stefan.majer@f-i-ts.de>
2022-11-10 10:35:39 +0100

    Adopt tenantfind mapper

--- api/rest/v1/tenant.go
+++ api/rest/v1/tenant.go
@@ -32,8 +32,10 @@ type (
 	}
 
 	TenantFindRequest struct {
-		Id   *string `json:"id,omitempty"`
-		Name *string `json:"name,omitempty"`
+		Id          *string           `json:"id,omitempty"`
+		Name        *string           `json:"name,omitempty"`
+		Paging      *Paging           `json:"paging,omitempty"`
+		Annotations map[string]string `json:"annotations,omitempty"`
 	}
 
 	TenantResponse struct {
@@ -42,5 +44,10 @@ type (
 
 	TenantListResponse struct {
 		Tenants []*TenantResponse `json:"tenants,omitempty"`
+	}
+
+	Paging struct {
+		Page  *uint64 `json:"page,omitempty"`
+		Count *uint64 `json:"count,omitempty"`
 	}
 )
