723fce20b29fb9f7aa87cfc4f420a29a882c97a7
Stefan Majer <stefan.majer@f-i-ts.de>
2024-02-23 13:13:48 +0100

    Fix mocks

--- pkg/client/mock.go
+++ pkg/client/mock.go
@@ -5,14 +5,16 @@ import (
 )
 
 type MockClient struct {
-	psc v1.ProjectServiceClient
-	tsc v1.TenantServiceClient
+	psc  v1.ProjectServiceClient
+	tsc  v1.TenantServiceClient
+	pmsc v1.ProjectMemberServiceClient
 }
 
-func NewMock(psc v1.ProjectServiceClient, tsc v1.TenantServiceClient) *MockClient {
+func NewMock(psc v1.ProjectServiceClient, tsc v1.TenantServiceClient, pmsc v1.ProjectMemberServiceClient) *MockClient {
 	return &MockClient{
-		psc: psc,
-		tsc: tsc,
+		psc:  psc,
+		tsc:  tsc,
+		pmsc: pmsc,
 	}
 }
 
@@ -21,6 +23,9 @@ func (c *MockClient) Close() error {
 }
 func (c *MockClient) Project() v1.ProjectServiceClient {
 	return c.psc
+}
+func (c *MockClient) ProjectMember() v1.ProjectMemberServiceClient {
+	return c.pmsc
 }
 
 func (c *MockClient) Tenant() v1.TenantServiceClient {
