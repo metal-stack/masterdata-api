68c1b2149ee5591c31ac27b4528b3f3a2dfe7540
Botond Gal <botond.gal@x-cellent.com>
2025-04-15 16:02:11 +0200

    Update mock interface

--- pkg/client/mock.go
+++ pkg/client/mock.go
@@ -9,14 +9,16 @@ type MockClient struct {
 	tsc  v1.TenantServiceClient
 	pmsc v1.ProjectMemberServiceClient
 	tmsc v1.TenantMemberServiceClient
+	vsc  v1.VersionServiceClient
 }
 
-func NewMock(psc v1.ProjectServiceClient, tsc v1.TenantServiceClient, pmsc v1.ProjectMemberServiceClient, tmsc v1.TenantMemberServiceClient) *MockClient {
+func NewMock(psc v1.ProjectServiceClient, tsc v1.TenantServiceClient, pmsc v1.ProjectMemberServiceClient, tmsc v1.TenantMemberServiceClient, vsc v1.VersionServiceClient) *MockClient {
 	return &MockClient{
 		psc:  psc,
 		tsc:  tsc,
 		pmsc: pmsc,
 		tmsc: tmsc,
+		vsc:  vsc,
 	}
 }
 
@@ -35,4 +37,7 @@ func (c *MockClient) Tenant() v1.TenantServiceClient {
 }
 func (c *MockClient) TenantMember() v1.TenantMemberServiceClient {
 	return c.tmsc
+}
+func (c *MockClient) Version() v1.VersionServiceClient {
+	return c.vsc
 }
